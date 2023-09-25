d74d9833653dd42dae7903f9c54275f803e984a9
Remco Haszing <remcohaszing@gmail.com>
2023-09-25 19:15:54 +0200

    Configure remark as default processor
    This is used as a fallback if remark can’t be resolved.
    
    Closes #6

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 import process from 'node:process'
+import {remark} from 'remark'
 import {createUnifiedLanguageServer} from 'unified-language-server'
 
 process.title = 'remark-language-server'
 
 createUnifiedLanguageServer({
   configurationSection: 'remark',
+  defaultProcessor: remark,
   ignoreName: '.remarkignore',
   packageField: 'remarkConfig',
   pluginPrefix: 'remark',
